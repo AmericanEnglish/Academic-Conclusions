b5b07052bc27ca813a6ecf125680bc7eac481d8f
Carlos Barajas <12barajasc@gmail.com>
2014-11-24 21:29:45 -0500

    Changed action terminal

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def mainloop():
     protag = Player('Admin Istrator', [], (0, 0))
     protag.person.append(Sword('aSword', 5, 3, (10, 11)))
     while True:
-        action = input('ADVENTURE TIME> ')
+        action = input('{} {}>'.format(protag.name[0], protag.name[1]))
         action = action.split()
         if  action[0].lower() == 'quit':
             return
