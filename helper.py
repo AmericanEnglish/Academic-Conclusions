ba19b3deddcf9f92f46f318b98351191167ad5d7
Carlos Barajas <12barajasc@gmail.com>
2014-12-01 17:36:27 -0500

    Changed file format for maps

--- helper.py
+++ helper.py
@@ -34,10 +34,10 @@ def writemaps():
     dimensions = input('Max X Y: ').strip().split()
     start = input('Start: ').strip().split()
     with open(outputfile, 'w') as fileout:
-        fileout.write('NAME {}\n'.format(mapname))
-        fileout.write('DIMX 0 {}\n'.format(dimensions[0]))
-        fileout.write('DIMY 0 {}\n'.format(dimensions[1]))
-        fileout.write('START {} {}\n'.format(start[0], start[1]))
+        fileout.write('NAME, {}\n'.format(mapname))
+        fileout.write('DIMX, 0, {}\n'.format(dimensions[0]))
+        fileout.write('DIMY, 0, {}\n'.format(dimensions[1]))
+        fileout.write('START, {}, {}\n'.format(start[0], start[1]))
         fileout.write('\n')
         answer = None
         while answer != 'done':
@@ -47,13 +47,13 @@ def writemaps():
                 name = input('Container name: ')
                 composition = input('Composition: ')
                 contents = input('Contents? (y/n): ')
-                fileout.write('CONT {} {} {} {}\n'.format(x, y, name, composition))
+                fileout.write('CONT, {}, {}, {}, {}\n'.format(x, y, name, composition))
                 if contents == 'y':
                     while True:
                         contentname = input('{}|Item Name: '.format(name))
                         contentcomp = input('Item Composition: ')
                         if contentname != 'done':
-                            fileout.write('INSI {} INTER {} {} {} {}\n'.format(name, x, y, contentname, contentcomp))
+                            fileout.write('INSI, {}, INTER, {}, {}, {}, {}\n'.format(name, x, y, contentname, contentcomp))
                         elif contentname == 'done':
                             break
             
@@ -67,18 +67,18 @@ def writemaps():
                 composition = input('Composition: ')
                 Doorkey = input('Door key (None/name): ')
                 if Doorkey.lower() == '':
-                    fileout.write('ROOM {} {} {} {} {} {}\n'.format(x, y, name, doornam, locked, composition))
+                    fileout.write('ROOM, {}, {}, {}, {}, {}, {}\n'.format(x, y, name, doornam, locked, composition))
                 else:
-                    fileout.write('ROOM {} {} {} {} {} {} {}\n'.format(x, y, name, doornam, locked, composition, Doorkey))
+                    fileout.write('ROOM, {}, {}, {}, {}, {}, {}, {}\n'.format(x, y, name, doornam, locked, composition, Doorkey))
                     keyquestion = input('Random gen key location?: ')
                     if keyquestion == 'n':
                         x2, y2 = input('Key x y: ').strip().split()
                         fileout.write('# Key location, Mutable\n')
-                        fileout.write('INTER GROUND {} {} {} {}\n'.format(x2, y2, Doorkey, composition))
+                        fileout.write('INTER, GROUND, {}, {}, {}, {}\n'.format(x2, y2, Doorkey, composition))
                     else:
                         x2, y2 = randint(0, int(dimensions[0])), randint(0,  int(dimensions[1]))
                         fileout.write('# Key location, Mutable\n')
-                        fileout.write('INTER GROUND {} {} {}\n'.format(x2, y2, Doorkey))
+                        fileout.write('INTER, GROUND, {}, {}, {}\n'.format(x2, y2, Doorkey))
                 fodder = input('{} Contents? (y/n): '.format(name))
                 if fodder == 'y':
                     while True:
@@ -86,13 +86,13 @@ def writemaps():
                         if objtype == 'cont':
                             contname = input('Container name: ')
                             composition = input('Composition: ')
-                            fileout.write('INSI {} CONT {} {} {} {}\n'.format(name, x, y, contname, composition)) 
+                            fileout.write('INSI, {}, CONT, {}, {}, {}, {}\n'.format(name, x, y, contname, composition)) 
                             if input('{} Contents? (y/n):'.format(contname)).lower() == 'y':    
                                 while True:
                                     contentname = input('Item Name: ')
                                     contentcomp = input('Item Composition: ')
                                     if contentname != 'done':
-                                        fileout.write('SPEC {} {} INTER {} {} {} {}\n'.format(name, contname, x, y, contentname, contentcomp))
+                                        fileout.write('SPEC, {}, {}, INTER, {}, {}, {}, {}\n'.format(name, contname, x, y, contentname, contentcomp))
                                     elif contentname == 'done':
                                         break
                         elif objtype == 'inter':
@@ -102,10 +102,10 @@ def writemaps():
                                 if contname == 'done':
                                     break
                                 elif contname != 'done':
-                                    fileout.write('INSI {} INTER {} {} {} {}\n'.format(name, x, y, contname, contentcomp))
+                                    fileout.write('INSI, {}, INTER, {}, {}, {}, {}\n'.format(name, x, y, contname, contentcomp))
                         elif objtype == 'npc':
                             npc = input('Filename: ')
-                            fileout.write('RNPC {} {} {} {}\n'.format(x, y, npc, name))
+                            fileout.write('RNPC, {}, {}, {}, {}\n'.format(x, y, npc, name))
                         else:
                             break
 
@@ -113,13 +113,12 @@ def writemaps():
                 name = input('Item name: ')
                 x, y = input('x y: ').split()
                 composition = input('Composition: ')
-                fileout.write('INTER GROUND {} {} {} {}\n'.format(x, y, name, composition))
+                fileout.write('INTER, GROUND, {}, {}, {}, {}\n'.format(x, y, name, composition))
 
             elif answer == 'npc':
                 x, y = input('x y:')
                 npc = input('Filename: ')
-                fileout.write('NPC {} {} {}\n'.format(x, y, npc))
+                fileout.write('NPC, {}, {}, {}\n'.format(x, y, npc))
 
             else:
                 break
-    convert_to(outputfile, outputfile + '.bak')
