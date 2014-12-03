72b4a69a07001634d23b4a7b6898e5485d07e064
Carlos Barajas <12barajasc@gmail.com>
2014-12-03 01:40:24 -0500

    Story added back in

--- main.py
+++ main.py
@@ -342,14 +342,14 @@ def score(protag):
 
 if __name__ == '__main__':
     print('')
-    # with open('intro', 'r') as intro:
-    #     print(intro.readline().strip())
-    #     for line in intro:
-    #         t1 = perf_counter()
-    #         t2 = t1 
-    #         while t2 - t1 < 2:
-    #             t2 = perf_counter()
-    #         print(line, end='')    
+    with open('intro', 'r') as intro:
+        print(intro.readline().strip())
+        for line in intro:
+            t1 = perf_counter()
+            t2 = t1 
+            while t2 - t1 < 2:
+                t2 = perf_counter()
+            print(line, end='')    
     choice = 'No'
     while choice.lower()[0] != 'y':
         name = input(' Can you at least tell me your name before I go? ')
