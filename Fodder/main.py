93369e1ee811fb808684c8f33e8756a5216608ab
Carlos Barajas <12barajasc@gmail.com>
2015-03-05 17:39:56 -0500

    Modifying and moving so game still works

---
+++ Fodder/main.py
@@ -0,0 +1,355 @@
+from player import *
+from maps import *
+from time import perf_counter
+from helper import helpcom
+from introduction import *
+
+
+map0 = Mapp('maps/map0')
+map1 = Mapp('maps/map1')
+map2 = Mapp('maps/map2')
+map3 = Mapp('maps/map3')
+map0.link(map1, (0, 0))
+map1.link(map0, (2, 0))
+map1.link(map2, (2, 2))
+map2.link(map1, (1, 0))
+map2.link(map3, (1, 2))
+map3.link(map2, (0, 0))
+
+def maploop(currentmap):
+    """(Mapp) -> None
+
+    These function is used for running movement and actions on a generic
+    Mapp object. The protag is stored in the global frame and is then
+    used and mutated in relation to the Mapp object and the commands that
+    are input by the user."""
+    inmap = True
+    while inmap:
+        action = input('={}=> '.format(currentmap.name))
+        action = action.lower().strip().split()
+        if len(action) > 1:
+            action = [action[0], ' '.join(action[1:])]
+        if len(action) < 1:
+            print('')
+        
+        elif  action[0].lower() == 'quit':
+            if input('Are you sure? (y/n): ').lower() == 'y':
+                return True
+        
+        elif action[0] == 'm':
+            protag.move(action[1])
+            if protag.pos[0] > currentmap.x or protag.pos[0] < 0:
+                # Provides a warning if the map is left
+                if protag.pos[0] > currentmap.x + 1 or protag.pos[0] < -1:
+                    print('You were eaten by wolves')
+                    return True
+                elif protag.pos[0] == currentmap.x + 1 or protag.pos[0] == -1:
+                    print('You have left the saftey of the {}, move farther and you might not move'.format(
+                                                        currentmap.name))
+                    print('at all. Quickly there is no time, head back from whence you came.\n')
+            elif protag.pos[1] > currentmap.y or protag.pos[1] < 0:
+                # Provides a warning if the map is left
+                if protag.pos[1] > currentmap.y + 1 or protag.pos[1] < -1:
+                    print('You were eaten by cannibals')
+                    return True
+                elif protag.pos[1] == currentmap.y + 1 or protag.pos[1] == -1:
+                    print('You have left the saftey of the {}, move farther and you might not move'.format(
+                                                        currentmap.name))
+                    print('at all. Quickly there is no time, move back from whence you came.\n')
+        
+        elif action[0] == 'pack':
+            protag.pack_view()
+        
+        elif action[0] == 'me':
+            protag.person_view()
+        
+        elif action[0] == 'put':
+            protag.put(action[1])
+        
+        elif action[0] == 'pull':
+            if len(protag.person) < 5:
+                protag.pull(action[1])
+            else:
+                print("As you try to pull {} from your pack, you can't seem to grab it")
+                print("perhaps you should put items into your pack first. Free up those hands")
+        
+        elif action[0] == 'examine' and len(action) == 2:
+            # check to make sure item / room / door in question is in the area
+            for item in currentmap.check(protag.pos)[0]:
+                if isinstance(item, Room) and 'door' in action[1]:
+                    item.door.examine()
+                    break
+                # if the item in question isn't a door it's a room
+                elif action[1] == item.name.lower():
+                    item.examine()
+                    break
+            # if item is neither a door or room, search's player's person
+            protag.examine(action[1])
+
+        
+        elif action[0] == 'enter' and len(action) > 1:
+            # checks for any rooms in the area that might be enterable
+            for item in currentmap.check(protag.pos)[0]:
+                # allows for room entry
+                if action[1] == item.name.lower() and isinstance(item, Room):
+                    roomloop(protag, item)
+                # allows for Mapps to be entered
+                elif action[1] == item.name.lower() and isinstance(item, Mapp):
+                    inmap = False
+                    protag.map = item
+                    protag.pos = item.start
+            print('')
+
+        elif action[0] == 'exit':
+            print(">You're already outside!<\n")
+
+        elif action[0] == 'look':
+            if protag.pos[0] < currentmap.x + 1 and protag.pos[0] >= 0:
+                if protag.pos[1] < currentmap.y + 1 and protag.pos[1] >= 0:
+                    print('>Around you see<')
+                    for item in currentmap.check(protag.pos)[0]:
+                        print(item.name)
+                    print('')
+                else:
+                    print('You took too long and were eaten by wolves AND cannibals')
+                    return True
+            else:
+                print('You took too long and were eaten by wolves AND cannibals')
+                return True
+            
+        elif action[0] == 'ground':
+            if currentmap.check(protag.pos)[1] == []:
+                print('>There is nothing on the ground<\n')
+            else:
+                print('>On the ground you see<')
+                for item in currentmap.check(protag.pos)[1]:
+                    print('{}'.format(item.name))
+                print('')
+
+        elif action[0] == 'pickup' and len(action) == 2:
+            for item in currentmap.check(protag.pos)[1]:
+                if action[1] == item.name.lower() and len(protag.person) < 5:
+                    # this will be put into the Player methods later, maybe
+                    protag.person.append(item)
+                    currentmap.check(protag.pos)[1].remove(item)
+                    print('You picked up {}\n'.format(item.name))
+                elif action[1] == item.name.lower() and len(protag.person) >= 5:
+                    print('You fumble the {} and it falls back on the ground.'.format(item.name))
+                    print('It would seem you are carrying too much in your hands to hold anymore.')
+
+        elif action[0] == 'drop':
+            for item in protag.person:
+                if action[1] == item.name.lower():
+                    # this will be put into the player methods later
+                    protag.person.remove(item)
+                    currentmap.check(protag.pos)[1].append(item)
+                    print('You dropped {} on the ground\n'.format(item.name))
+        
+        elif action[0] == 'talk':
+            # if user types wrong name somevar will help print a newline
+            somevar = 0
+            if len(action) > 1:
+                for item in currentmap.check(protag.pos)[0]:
+                    somevar += 1
+                    if isinstance(item, NPC) and action[1] == item.name.lower():
+                        #somevar now knows an NPC was found and no extra \n
+                        somevar -= 1
+                        print('<>{}:\n{}'.format(item.name, item.talk(protag)))
+                if somevar == len(currentmap.check(protag.pos)[0]):
+                    print('')
+            # if just talk is typed, protag talks to all NPCs in area.   
+            else:
+                somevar = 0
+                for item in currentmap.check(protag.pos)[0]:
+                    somevar += 1
+                    if isinstance(item, NPC):
+                        somevar -= 1
+                        print('<>{}:\n{}'.format(item.name, item.talk(protag)))
+                if somevar == len(currentmap.check(protag.pos)[0]):
+                    print('Not a valid command, type help for help\n')
+        
+        elif action[0] == 'take':
+            # take requires a second marker called from. This requiers action
+            # to be reconfigured
+            if 'from' in action:
+                action = ' '.join(action).split()
+                action = [action[0],
+                        ' '.join(action[1:action.index('from')]),
+                        ' '.join(action[action.index('from') + 1:])]
+                for item in currentmap.contents[protag.pos][0]:
+                    if action[2] == item.name.lower():
+                        # will be added as table/object method later
+                        for thing in item.contents:
+                            if thing.name.lower() == action[1]:
+                                protag.person.append(thing)
+                                item.contents.remove(thing)
+                                print('You took {} from {}'.format(thing.name, item.name))
+        
+        elif action[0] == 'help' and len(action) == 2:
+            print(helpcom(action[1]))
+        
+        else:
+            print('Not a valid command, type help for help\n')
+    main(protag, protag.map)
+
+def roomloop(protag, currentroom):
+    """(Player, Room) -> None
+
+    This function is used for a player's interaction with the room and its 
+    contents. Although some 'action' words are the same the objects some
+    interactions are not exactly the same."""
+    protag.room = currentroom
+    #If the door is locked they are 'kicked' from the room
+    if not currentroom.door.open(protag):
+        return
+    print("You entered {}\n".format(currentroom.name))
+    while True:
+        action = input('={}=> '.format(currentroom.name))
+        action = action.lower().strip().split()
+        if len(action) > 1:
+            action = [action[0], ' '.join(action[1:])]
+        
+        if len(action) < 1:
+            print('')
+
+        elif action[0] == 'pack':
+            protag.pack_view()
+        
+        elif action[0] == 'me':
+            protag.person_view()
+        
+        elif action[0] == 'put':
+            protag.put(action[1])
+        
+        elif action[0] == 'pull':
+            protag.pull(action[1])
+        
+        elif action[0] == 'examine' and len(action) == 2:
+            for item in currentroom.contents[0]:
+                if action[1] in item.name.lower():
+                    item.examine()
+                    break
+            protag.examine(action[1])
+            print('')
+        
+        elif action[0] == 'enter':
+            print(">You're already in the {}<\n".format(currentroom.name))
+        
+        elif action[0] == 'exit':
+            print(">You exit the {}<\n".format(currentroom.name))
+            return
+
+        elif action[0] == 'look':
+            print('>Around you see<')
+            for item in currentroom.contents[0]:
+                print(item.name)
+            print('')
+        
+        elif action[0] == 'ground':
+            if currentroom.contents[1] == []:
+                print('>There is nothing on the ground<\n')
+            else:
+                print('>On the ground you see<')
+                for item in currentroom.contents[1]:
+                    print('{}'.format(item.name))
+                print('')
+
+        elif action[0] == 'pickup':
+            for item in currentroom.contents[1]:
+                if action[1] == item.name.lower():
+                    protag.person.append(item)
+                    currentroom.contents[1].remove(item)
+                    print('>You picked up {}<\n'.format(item.name))
+
+        elif action[0] == 'drop':
+            for item in protag.person:
+                if action[1] == item.name.lower():
+                    protag.person.remove(item)
+                    currentroom.contents[1].append(item)
+                    print('>You dropped {} on the ground<'.format(item.name))
+            print('')
+        
+        elif action[0] == 'take':
+            # take requires a second marker called from. This requiers action
+            # to be reconfigured
+            if 'from' in action:
+                action = ' '.join(action).split()
+                action = [action[0],
+                        ' '.join(action[1:action.index('from')]),
+                        ' '.join(action[action.index('from') + 1:])]
+                for item in currentroom.contents[0]:
+                    if action[2] == item.name.lower():
+                        # will be added as table/object method later
+                        for thing in item.contents:
+                            if thing.name.lower() == action[1]:
+                                protag.person.append(thing)
+                                item.contents.remove(thing)
+                                print('You took {} from {}'.format(thing.name, item.name))
+            print('')
+        
+        elif action[0] == 'talk':
+            # if user types wrong name somevar will help print a newline
+            somevar = 0
+            if len(action) > 1:
+                for item in currentroom.contents[0]:
+                    somevar += 1
+                    if isinstance(item, NPC) and action[1] == item.name.lower():
+                        #somevar now knows an NPC was found and no extra \n
+                        somevar -= 1
+                        print('<>{}:\n{}'.format(item.name, item.talk(protag)))
+                if somevar == len(currentroom.contents[0]):
+                    print('')
+            # if just talk is typed, protag talks to all NPCs in area.   
+            else:
+                somevar = 0
+                for item in currentroom.contents[0]:
+                    somevar += 1
+                    if isinstance(item, NPC):
+                        somevar -= 1
+                        print('<>{}:\n{}'.format(item.name, item.talk(protag)))
+                if somevar == len(currentroom.contents[0]):
+                    print('')
+
+        elif action[0] == 'help' and len(action) == 2:
+            print(helpcom(action[1]))
+        
+        else:
+            print('Not a valid command, type help for help\n')
+
+
+
+def main(protag, startingmap):
+    protag.map = startingmap
+    protag.pos = protag.map.start
+    death = False
+    while death != True:
+        death = maploop(protag.map)
+    print('----Score: {}----\n'.format(score(protag)))
+    answer = input('Try again?: ')
+    if answer[0].lower() == 'y':
+        main(protag, map0)
+    else:
+        print('Better luck next time!')
+
+def score(protag):
+    score = 0
+    for item in protag.person:
+        print('{}: {}'.format(str(item), materialvalue[item.madeof]))
+        score += materialvalue[item.madeof]
+    for item in protag.pack:
+        print('{}: {}'.format(str(item), materialvalue[item.madeof]))
+        score += materialvalue[item.score]
+    print('Goodness Points: {}'.format(protag.score))
+    return score + protag.score
+
+materialvalue = {
+                'wood':10, 'stone':15, 'metal': 20, 'gold':30, 'flesh':-30,
+                'bone':-20, 'meat':-10, 'evil':-5, 'curse':-50, 'cat':0,
+                'paper': 40, 'glass': 20, 'platinum': 100, 'cursed':-50,
+                'cloth':15
+                }
+
+if __name__ == '__main__':
+    name = introduction()
+    protag = Player(name, map0.start)
+    main(protag, map0)
---
+++ Fodder/maps.py
@@ -0,0 +1,278 @@
+class Mapp:
+    def __init__(self, filename):
+            #contents is a dict of tuple: list objects
+            # {(0, 0):[[Room, Enemey][Ground Things, Here]], (0,1): [[],[]]} 
+        running = {}
+        with open(filename, 'r') as somefile:
+            self.name = somefile.readline().strip().split(',')[1].strip()
+            
+            nextline = somefile.readline().strip().split(',')
+            dimensionx = (int(nextline[1].strip()), int((nextline[2].strip())))
+            self.x = dimensionx[1]
+
+            nextline = somefile.readline().strip().split(',')
+            dimensiony = (int(nextline[1].strip()), int(nextline[2].strip()))
+            self.y = dimensiony[1]
+
+            nextline = somefile.readline().strip().split(',')
+            self.start = (int(nextline[1].strip()), int(nextline[2].strip()))
+            #generates empty map contents and all the coordinates
+            for x in range(dimensionx[1] + 1):
+                for y in range(dimensiony[1] + 1):
+                    running[(x , y)] = [[],[]]
+
+            for aline in somefile:
+                aline = aline.strip().split(',')
+                line = []
+                for item in aline:
+                    line.append(item.strip())
+
+                if line[0] == 'ROOM':
+                    # ROOM X Y NAME DOORNAME LOCKED? COMP DOORKEY
+                    #  0   1 2  3      4        5     6      7
+                    if len(line) == 8:
+                        doorkey = line[7]
+                    else:
+                        doorkey = None
+                    x, y = int(line[1]), int(line[2])
+                    running[(x, y)][0].append(Room(line[3], [[],[]], Door(line[4], line[5].isalpha(), line[6], doorkey)))
+                
+                elif line[0] == 'INTER':
+                    # INTER GROUND X Y NAME COMP
+                    #   0     1    2 3  4    5
+                    if line[1] == 'GROUND':
+                        x, y = int(line[2]), int(line[3])
+                        name = line[4]
+                        composition = line[5]
+                        running[(x, y)][1].append(Interactable(name, composition))
+                    else:
+                        # INTER X Y NAME COMP
+                        #   0   1 2  3    4
+                        x = int(line[1])
+                        y = int(line[2])
+                        name = line[3]
+                        composition = line [4]
+                        running[(x, y)][0].append(Interactable(name, composition))
+
+                elif line[0] == 'NPC':
+                    # NPC X Y FILE
+                    #  0  1 2  3
+                    x, y = int(line[1]), int(line[2])
+                    running[(x, y)][0].append(NPC(line[3]))
+
+                elif line[0] == 'CONT':
+                    # CONT X Y NAME COMP
+                    #  0   1 2  3    4
+                    x, y = int(line[1]), int(line[2])
+                    running[(x, y)][0].append(Container(line[3], line[4], []))
+
+                elif line[0] == 'INSI':
+                    # INSI CONTNAME OBJCLASS X Y NAME COMP
+                    #   0     1       2      3 4  5    6 
+                    x, y = int(line[3]), int(line[4])
+                    for item in running[(x, y)][0]:
+                        if item.name.lower() == line[1].lower():
+                            # Puts interactable on ground in room
+                            if isinstance(item, Room) and line[2] == 'INTER':
+                                item.contents[1].append(Interactable(line[5], line[6]))
+                                break
+                            # Puts container inside room
+                            elif isinstance(item, Room) and line[2] == 'CONT':
+                                item.contents[0].append(Container(line[5], line[6], []))
+                                break
+                            # Puts interactable in container on map
+                            elif isinstance(item, Container):
+                                item.contents.append(Interactable(line[5], line[6]))
+                                break
+
+                elif line[0] == 'SPEC':
+                    # SPEC ROOMNAME CONTNAME INTER X Y NAME COMP
+                    #  0      1        2       3   4 5  6    7
+                    x, y = int(line[4]), int(line[5])
+                    for item in running[(x, y)][0]:
+                        if item.name.lower() == line[1].lower():
+                            for entity in item.contents[0]:
+                                if entity.name.lower() == line[2].lower():
+                                    entity.contents.append(Interactable(line[6], line[7]))
+                                    break
+                            break
+
+                elif line[0] == 'RNPC':
+                    # RNPC X Y FILE ROOMNAME
+                    #  0  1 2   3    4      
+                    x, y = int(line[1]), int(line[2])
+                    for item in running[(x, y)][0]:
+                        if isinstance(item, Room) and item.name.lower() == line[4].lower():
+                            item.contents[0].append(NPC(line[3]))
+        self.contents = running
+    def check(self, pos):
+        return self.contents[pos]
+
+    def link(self, somemap, coordinates):
+        """(Mapp, Mapp, tuple of ints) -> None
+
+        Links two existing maps together so that you can travel from one 
+        map to another using the enter function in the main.py"""
+        self.contents[coordinates][0].append(somemap)
+
+    def examine(self):
+        print('You see a path leading to a {}'.format(self.name))
+
+class Room:
+    def __init__(self, name, contents, door):
+        self.name = name
+        #self.num = num
+        self.contents = contents
+        self.door = door
+
+    def examine(self):
+        print("You examine the {}, it is solid maybe look at the {}".format(
+                    self.name, self.door.name))
+
+
+class Interactable:
+    
+    def __init__(self, name, composition):
+        #(self, name, weight, composition, num)
+        self.name = name
+        #self.weight = weight
+        self.madeof = composition
+        #self.num = num
+
+    def examine(self):
+        print("You examine the {}, it is made of {}\n".format(self.name, self.madeof))
+
+    def __str__(self):
+        return '{} {}'.format(self.madeof, self.name).title()
+
+
+class Door(Interactable):
+    
+    def __init__(self, name, locked, composition, key=None):
+        super().__init__(name, composition)
+        self.locked = locked
+        self.key = key
+        if locked:
+            self.lcked = 'locked'
+        else:
+            self.lcked = 'unlocked'
+    
+    def open(self, protag):
+        if self.locked:
+            for item in protag.person:
+                if self.key.lower() == str(item).lower():
+                    self.locked = False
+                    self.lcked = 'unlocked'
+                    print('You unlocked the {} with {}'.format(self.name, self.key))
+                    return True
+
+            print('The {} is locked'.format(self.name))
+            return False
+        else:
+            return True
+
+    def examine(self):
+        print('Door appears to made of {} and is {}'.format(self.madeof, self.lcked))
+
+
+class Container(Interactable):
+    def __init__(self, name, composition, contents):
+        self.contents = contents
+        self.madeof = composition
+        self.name = name
+
+    def examine(self):
+        if self.contents == []:
+            print('{} is made of {} and is empty'.format(self.name, self.madeof))
+        else:
+            print('>Contents of the {} {} are<'.format(self.madeof, self.name))
+            for item in self.contents:
+                print('{}'.format(item.name))
+
+class NPC():
+    """Generates NPC objects"""
+    def __init__(self, dialogfile):
+        """(str, file.ext, str) -> None
+
+        NPCs need a name and a file containing their dialogue trees. The
+        dialogues in the file will be in the correct order such that the
+        first talk is informative, the second is either a small reminder
+        or perhaps exhibits annoyance of the character and in some cases
+        a refusal to repeat.
+
+        Coversations that are trigger by keyitems are at the end are marked
+        by the word KEY and all dialogue should be typed as to how it will
+        appear in the interpreter. NPCs without key items will ignore all
+        key item dialogue and just act as direction givers.
+
+        NAME name
+        DIALOGUE pew pew pew pew
+        NEWTXT
+        DIALOGUE meow meow meow
+        KEYITEM KITTY
+        KEY
+        Thank you for finding my animal
+
+        """
+        self.dialog = []
+        temp = ''
+        key = False
+        self.convo = 0
+        self.give = None
+        self.keyitem = None
+        # dissects character's dialogue file
+        with open(dialogfile, 'r') as scrapfile:
+            for line in scrapfile:
+                line = line.split()
+                if line[0] == 'NAME':
+                    self.name = ' '.join(line[1:]).strip()
+                elif line[0] == 'NEWTXT':
+                    self.dialog.append(temp)
+                    temp = ''
+                elif line[0] == 'GIVE':
+                    self.give = Interactable(line[1], line[2])
+                elif line[0] == 'KEYITEM':
+                    self.keyitem = ' '.join(line[1:])
+                elif line[0] == 'DIALOG':
+                    temp += '{}\n'.format(' '.join(line[1:]))
+                elif line[0] == 'KEY':
+                    key = True
+                    break
+            self.dialog.append(temp)
+            if key:
+                self.keyconvo = scrapfile.read()
+
+    def talk(self, protag):
+        # if NPC has key item checks player's person, display new dialogue
+        if self.keyitem == 'glass key 5':
+            for item in protag.person:
+                if str(item).lower() == self.keyitem.lower():
+                    protag.person.append(self.give)
+                    print("You've obtained a {}".format(self.give))
+                    print(self.keyconvo)
+                    print(score(protag))
+                    print('Run game again to play.')
+                    exit()
+        
+        if self.keyitem != None:
+            for item in protag.person:
+                if str(item).lower() == self.keyitem.lower():
+                    protag.person.remove(item)
+                    self.dialog = ['Thank you {} for your help\n'.format(protag.name)]
+                    protag.score += materialvalue[item.madeof] + 10
+                    if self.give != None:
+                        protag.person.append(self.give)
+                        print("You've obtained a {}".format(str(self.give)))
+                    return self.keyconvo
+        # if player has item in question they are thanked and no additional
+        # dialogue needed
+        if self.dialog == ['Thank you so much for your help\n']:
+            return self.dialog[0]
+            # Allows for several 'hint' like dialogs
+        elif self.convo < len(self.dialog) - 1:
+            self.convo += 1
+            return self.dialog[self.convo - 1]
+        return self.dialog[self.convo]
+
+    def examine(self):
+        print('They step away from you leery of your intentions')
