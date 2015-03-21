f5a0d6943a229c09ed20b41c5bae5a2e36201028
Carlos Barajas <12barajasc@gmail.com>
2015-03-20 22:20:06 -0400

    Changed table formats

--- Fodder/tables.sql
+++ Fodder/tables.sql
@@ -1,56 +1,36 @@
 --Player's Personal Items
-CREATE TABLE inventory
-(
-    name VARCHAR(20),
-    id INTEGER,
-    PRIMARY KEY (name, id)
-);
-
-CREATE TABLE pack
-(
-	name VARCHAR(20)
-	id INTEGER,
-	PRIMARY KEY (name, id)
-);
-
 CREATE TABLE items
 (
-	name VARCHAR(20) NOT NULL,
-	id INTEGER NOT NULL,
-	x INTEGER,
-	y INTEGER,
-	map INTEGER,
-	value MONEY,
-	PRIMARY KEY (name, id, x, y),
-	UNIQUE (id)
+    name VARCHAR(20) NOT NULL,
+    id INTEGER NOT NULL,
+    x INTEGER,
+    y INTEGER,
+    map INTEGER,
+    description VARCHAR,
+    -- value MONEY,
+    PRIMARY KEY (name)
 );
 
 CREATE TABLE containers
 ( 
-	id INTEGER,
-	locked BOOLEAN,
-	x INTEGER NOT NULL,
-	y INTEGER NOT NULL,
-	reqkey INTEGER,
-	map INTEGER, 
-	PRIMARY KEY (id, locked),
-	FOREIGN KEY (reqkey)
-		REFERENCES items (id)
+    id INTEGER,
+    locked BOOLEAN,
+    x INTEGER NOT NULL,
+    y INTEGER NOT NULL,
+    keys_item_id INTEGER,
+    map INTEGER, 
+    PRIMARY KEY (id),
+    FOREIGN KEY (keys_item_id)
+        REFERENCES items (id)
 );
 
-CREATE TABLE rooms
+CREATE TABLE npcconditionals
 (
-	map INTEGER,
-	PRIMARY KEY (map)
+    npc_name VARCHAR(20),
+    condition VARCHAR,
+    action VARCHAR,
+    PRIMARY KEY (npc_name, condition)
 );
 
-CREATE TABLE npc
-(
-	name VARCHAR(20)
-	PRIMARY KEY (name)
-);
-
--- DROP TABLE inventory;
--- DROP TABLE pack;
 -- DROP TABLE items;
 -- DROP TABLE containers;
