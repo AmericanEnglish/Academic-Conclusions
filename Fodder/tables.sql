92cc4eb918715ae978026c295191bacb1b0e6809
Carlos Barajas <12barajasc@gmail.com>
2015-03-20 22:59:36 -0400

    Keeping static values in one unchanging table

--- Fodder/tables.sql
+++ Fodder/tables.sql
@@ -1,14 +1,16 @@
 --Player's Personal Items
 CREATE TABLE items
 (
-    name VARCHAR(20) NOT NULL,
     id INTEGER NOT NULL,
+    name VARCHAR(20) NOT NULL,
     x INTEGER,
     y INTEGER,
     map INTEGER,
     description VARCHAR,
     -- value MONEY,
-    PRIMARY KEY (name)
+    PRIMARY KEY (id)
+    FOREIGN KEY (name)
+        REFERENCES item_worth (name)
 );
 
 CREATE TABLE containers
@@ -32,5 +34,11 @@ CREATE TABLE npcconditionals
     PRIMARY KEY (npc_name, condition)
 );
 
+CREATE TABLE item_worth
+(
+    name VARCHAR(20)
+    price MONEY
+    PRIMARY KEY (name)
+);
 -- DROP TABLE items;
 -- DROP TABLE containers;
