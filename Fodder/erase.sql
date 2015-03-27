7227ce75023bc1b196a72e34c2a24f0d8fc76cda
Carlos Barajas <12barajasc@gmail.com>
2015-03-27 17:02:33 -0400

    Added a file for mass dropping

---
+++ Fodder/erase.sql
@@ -0,0 +1,10 @@
+ALTER TABLE items
+    DROP CONSTRAINT container_must_exist;
+
+DROP TABLE npc_dialogue;
+DROP TABLE npc_conditionals;
+DROP TABLE npcs;
+DROP TABLE containers;
+DROP TABLE items;
+DROP TABLE maps;
+DROP TABLE item_worth;
