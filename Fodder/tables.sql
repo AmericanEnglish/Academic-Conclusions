dc834f425d03801298517cc59080795aaf73892e
Carlos Barajas <12barajasc@gmail.com>
2015-03-27 16:55:13 -0400

    ALTER TABLE so everything can created without error

--- Fodder/tables.sql
+++ Fodder/tables.sql
@@ -1,3 +1,21 @@
+-- The first two static tables
+CREATE TABLE maps
+(
+    name VARCHAR(20),
+    max_x INTEGER,
+    max_y INTEGER,
+    description VARCHAR,
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE item_worth
+(
+    name VARCHAR(20),
+    value MONEY,
+    PRIMARY KEY (name)
+);
+
+-- Table of changing values
 CREATE TABLE items
 (
     id INTEGER NOT NULL,
@@ -5,18 +23,17 @@ CREATE TABLE items
     x INTEGER, -- If both x and y are NULL 
     y INTEGER, -- then item must be on the ground in a room
     map_name VARCHAR(20),
-    container_id INTEGER, -- If NULL then item is on ground
+    -- container_id INTEGER, -- If NULL then item is on ground
     description VARCHAR,
     PRIMARY KEY (id),
     FOREIGN KEY (name)
         REFERENCES item_worth (name),
-    FOREIGN KEY (container_id)
-        REFERENCES containers (id),
+    -- FOREIGN KEY (container_id)
+    --     REFERENCES containers (id),
     FOREIGN KEY (map_name)
         REFERENCES maps (name)
 );
 
--- These tables will not be altered after initial insertion
 CREATE TABLE containers
 ( 
     id INTEGER,
@@ -33,6 +50,31 @@ CREATE TABLE containers
     FOREIGN KEY (parent_container_id)
         REFERENCES containers (id)
         ON UPDATE SET NULL,
+    FOREIGN KEY (map_name)
+        REFERENCES maps (name)
+);
+
+-- Alter table is required because of FK overlaps
+ALTER TABLE items
+	ADD container_id INTEGER;
+
+ALTER TABLE items
+	ADD CONSTRAINT container_must_exist
+		FOREIGN KEY (container_id)
+        	REFERENCES containers (id);
+
+-- npcs are static and unchanging
+CREATE TABLE npcs
+(
+    name VARCHAR(20),
+    x INTEGER, -- If x, y are null then the npc is
+    y INTEGER, -- in a room.
+    map_name VARCHAR(20),
+    room_id INTEGER,
+    description VARCHAR,
+    PRIMARY KEY (name),
+    FOREIGN KEY (room_id)
+        REFERENCES containers (id),
     FOREIGN KEY (map_name)
         REFERENCES maps (name)
 );
@@ -57,36 +99,7 @@ CREATE TABLE npc_dialogue
         REFERENCES npcs (name)
 );
 
-CREATE TABLE npcs
-(
-    name VARCHAR(20),
-    x INTEGER, -- If x, y are null then the npc is
-    y INTEGER, -- in a room.
-    map_name VARCHAR(20),
-    room_id INTEGER,
-    description VARCHAR,
-    PRIMARY KEY (name),
-    FOREIGN KEY (room_id)
-        REFERENCES containers (id),
-    FOREIGN KEY (map_name)
-        REFERENCES maps (name)
-);
 
-CREATE TABLE maps
-(
-    name VARCHAR(20),
-    max_x INTEGER,
-    max_y INTEGER,
-    description VARCHAR,
-    PRIMARY KEY (name)
-);
-
-CREATE TABLE item_worth
-(
-    name VARCHAR(20),
-    value MONEY,
-    PRIMARY KEY (name)
-);
 
 --DROP TABLE containers;
 --DROP TABLE items;
