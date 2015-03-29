1185a5f89a014f2d9e4c4c53fe8cf67ac7eb74af
Carlos Barajas <12barajasc@gmail.com>
2015-03-28 23:30:39 -0400

    Inventory & backpack

---
+++ Fodder/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO containers (id, name, x, y, map_name, parent_container_id, description, unlock_item_id)
+    VALUES
+        (1, 'inventory', NULL, NULL, NULL, NULL, 'Your bare hands and pockets', NULL ),
+        (2, 'backpack' , NULL, NULL, NULL, NULL, 'Your pack of many things'   , NULL);
